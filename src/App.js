a7e3648f4aea4afc918692abcd952717c7985b24
Kamal1701 <kk17kannan@yahoo.com>
2025-08-05 11:04:35 +0530

    understand useEffect

--- src/App.js
+++ src/App.js
@@ -2,27 +2,32 @@ import AddItem from "./AddItem";
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchItem from "./SearchItem";
 
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem("shoppinglist"))
+    JSON.parse(localStorage.getItem("shoppinglist")) || []
   );
 
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem("shoppinglist", JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem("shoppinglist", JSON.stringify(items));
+  }, [items]);
+
+  // const setAndSaveItems = (newItems) => {
+  //   setItems(newItems);
+  //   localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+  // };
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSaveItems(listItems);
+    // setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
@@ -30,12 +35,14 @@ function App() {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSaveItems(listItems);
+    // setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSaveItems(listItems);
+    // setAndSaveItems(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
---
+++ src/tut10/Input.js
@@ -0,0 +1,31 @@
+import colorNames from "colornames";
+
+const Input = ({
+  colorValue,
+  setColorValue,
+  setHexValue,
+  isDarkText,
+  setIsDarkText,
+}) => {
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <label>Add color name:</label>
+      <input
+        autoFocus
+        type="text"
+        placeholder="Enter the color name"
+        required
+        value={colorValue}
+        onChange={(e) => {
+          setColorValue(e.target.value);
+          setHexValue(colorNames(e.target.value));
+        }}
+      ></input>
+      <button type="button" onClick={() => setIsDarkText(!isDarkText)}>
+        Toggle text color
+      </button>
+    </form>
+  );
+};
+
+export default Input;
